dc98329311daba3674f903ef2602f29165ff5ea4
Jack Sharkey <jsharkey90@gmail.com>
2021-03-09 04:53:37 -0500

    finishing first draft of 'gulpfile.js'; updated 'package.json' and 'package-lock.json' accordingly

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,48 @@
-import gulp from 'gulp';
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+const gulp = require('gulp');
+const mergeStream = require('merge-stream');
+const postcss = require('gulp-postcss');
+const rename = require('gulp-rename');
+const sass = require('gulp-sass');
+
+function onError(error) {
+  console.log(error.message);
+  this.emit('end');
+}
 
 gulp.task('css', () => {
-  const plugins = [
-    autoprefixer({browsers: ['last 1 version']}),
-    cssnano()
-  ];
-  return gulp.src('./src/init.scss')
-    .pipe(postcss(plugins))
-    .pipe(gulp.dest('./dist'));
+  return mergeStream(
+    gulp.src('./src/init.scss')
+      .pipe(sass())
+      .on('error', onError)
+      .pipe(postcss([
+        autoprefixer(),
+        cssnano({
+          preset: ['default', {
+            normalizeWhitespace: 'true',
+          }],
+        })
+      ]))
+      .pipe(rename({
+        suffix: '.min',
+        extname: '.css'
+      }))
+      .pipe(gulp.dest('./dist')),
+    gulp.src('./src/init.scss')
+      .pipe(sass())
+      .on('error', onError)
+      .pipe(postcss([
+        autoprefixer(),
+        cssnano({
+          preset: ['default', {
+            normalizeWhitespace: 'false',
+          }],
+        })
+      ]))
+      .pipe(rename({
+        extname: '.css'
+      }))
+      .pipe(gulp.dest('./dist'))
+  );
 });
