16b6adc7fee9de4c19d8b2eb3a57999d3ca1b1c8
Jack Sharkey <jsharkey90@gmail.com>
2022-06-05 18:29:43 -0400

    updating gulpfile to use modern gulp v4 syntax

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 const gulp = require('gulp');
-const mergeStream = require('merge-stream');
 const postcss = require('gulp-postcss');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass')(require('sass'));
@@ -11,38 +10,43 @@ function onError(error) {
   this.emit('end');
 }
 
-gulp.task('css', () => {
-  return mergeStream(
-    gulp.src('./src/init.scss')
-      .pipe(sass())
-      .on('error', onError)
-      .pipe(postcss([
-        autoprefixer(),
-        cssnano({
-          preset: ['default', {
-            normalizeWhitespace: true,
-          }],
-        })
-      ]))
-      .pipe(rename({
-        suffix: '.min',
-        extname: '.css'
-      }))
-      .pipe(gulp.dest('./dist')),
-    gulp.src('./src/init.scss')
-      .pipe(sass())
-      .on('error', onError)
-      .pipe(postcss([
-        autoprefixer(),
-        cssnano({
-          preset: ['default', {
-            normalizeWhitespace: false,
-          }],
-        })
-      ]))
-      .pipe(rename({
-        extname: '.css'
-      }))
-      .pipe(gulp.dest('./dist'))
-  );
-});
+function minified() {
+  return gulp.src('./src/init.scss')
+    .pipe(sass())
+    .on('error', onError)
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano({
+        preset: ['default', {
+          normalizeWhitespace: true,
+        }],
+      }),
+    ]))
+    .pipe(rename({
+      suffix: '.min',
+      extname: '.css',
+    }))
+    .pipe(gulp.dest('./dist'));
+}
+
+function expanded() {
+  return gulp.src('./src/init.scss')
+    .pipe(sass())
+    .on('error', onError)
+    .pipe(postcss([
+      autoprefixer(),
+      cssnano({
+        preset: ['default', {
+          normalizeWhitespace: false,
+        }],
+      }),
+    ]))
+    .pipe(rename({
+      extname: '.css',
+    }))
+    .pipe(gulp.dest('./dist'));
+}
+
+const compile = gulp.parallel(minified, expanded);
+
+exports.default = compile;
